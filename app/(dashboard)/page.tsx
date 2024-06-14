318ef9a7030334a2f778cd4cba091e5d23b4fc01
Mantas <defixass@gmail.com>
2024-06-14 20:54:38 +0300

    clerk signin/ signup pages.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
+
+import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button className='bg-red-600'>AUTH CODE</Button>
+        <UserButton afterSignOutUrl='/' />
+
     </main>
   );
 }
